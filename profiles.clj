637dafee5a30a4d2f8cbba90c23c729d30ad798e
John Flinchbaugh <john@hjsoft.com>
2019-03-28 02:44:22 -0400

    lein profiles.clj

--- profiles.clj
+++ profiles.clj
@@ -1,1 +1,9 @@
-{:user {:plugins [[com.jakemccrary/lein-test-refresh "0.23.0"]]}}
+{:user
+ {
+  :plugins [
+    [lein-cloverage "1.1.1"]
+    [lein-ancient "0.6.15"]
+    [lein-kibit "0.1.6"]
+  ]
+ }
+}
