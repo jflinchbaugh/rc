cc2774b1b7ee6f84686af42e1c132eece5a84d50
John Flinchbaugh <john@hjsoft.com>
2019-09-19 14:18:58 -0400

    reformat + add lein-nvd

--- profiles.clj
+++ profiles.clj
@@ -1,10 +1,8 @@
 {:user
  {
   :plugins [
-    [lein-cloverage "1.1.1"]
-    [lein-ancient "0.6.15"]
-    [lein-kibit "0.1.6"]
-    [com.jakemccrary/lein-test-refresh "0.24.0"]
-  ]
- }
-}
+            [lein-cloverage "1.1.1"]
+            [lein-ancient "0.6.15"]
+            [lein-kibit "0.1.7"]
+            [com.jakemccrary/lein-test-refresh "0.24.1"]
+            [lein-nvd "1.3.0"]]}}
