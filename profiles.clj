c80cbd8cec5f308a8ddc1c5d4a5659fba1718b0b
John Flinchbaugh <john@hjsoft.com>
2020-03-02 20:17:24 -0500

    trim jvm props from lein

--- profiles.clj
+++ profiles.clj
@@ -5,4 +5,5 @@
             [lein-ancient "0.6.15"]
             [lein-kibit "0.1.7"]
             [com.jakemccrary/lein-test-refresh "0.24.1"]
-            [lein-nvd "1.3.0"]]}}
+            [lein-nvd "1.3.0"]]
+  :jvm-opts ^:replace []}}
