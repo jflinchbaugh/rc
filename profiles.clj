7e7ace1aecb1f988203c8aa9a9c0c6b7d82af00c
John Flinchbaugh <john@hjsoft.com>
2020-08-25 02:54:33 -0400

    upgrade profile libs

--- profiles.clj
+++ profiles.clj
@@ -1,9 +1,10 @@
 {:user
  {
   :plugins [
-            [lein-cloverage "1.1.1"]
+            [lein-cloverage "1.1.2"]
             [lein-ancient "0.6.15"]
             [lein-kibit "0.1.7"]
             [com.jakemccrary/lein-test-refresh "0.24.1"]
-            [lein-nvd "1.3.0"]]
+            [lein-nvd "1.4.1"]
+            [com.livingsocial/lein-dependency-check "1.1.4"]]
   :jvm-opts ^:replace []}}
