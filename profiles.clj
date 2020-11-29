0bcdd62a55fa430c874f264927c736c9735eea2c
John Flinchbaugh <john@hjsoft.com>
2020-11-28 23:35:42 -0500

    upgrade kibit

--- profiles.clj
+++ profiles.clj
@@ -3,7 +3,7 @@
   :plugins [
             [lein-cloverage "1.1.2"]
             [lein-ancient "0.6.15"]
-            [lein-kibit "0.1.7"]
+            [lein-kibit "0.1.8"]
             [com.jakemccrary/lein-test-refresh "0.24.1"]
             [lein-nvd "1.4.1"]
             [com.livingsocial/lein-dependency-check "1.1.4"]]
