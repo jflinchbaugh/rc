0f63a192a4802dc168a83184a7d72cddbd43d82f
John Flinchbaugh <john@hjsoft.com>
2021-05-04 10:57:45 -0400

    upgrade lein tools

--- profiles.clj
+++ profiles.clj
@@ -1,10 +1,10 @@
 {:user
  {
   :plugins [
-            [lein-cloverage "1.1.2"]
-            [lein-ancient "0.6.15"]
+            [lein-cloverage "1.2.2"]
+            [lein-ancient "0.7.0"]
             [lein-kibit "0.1.8"]
             [com.jakemccrary/lein-test-refresh "0.24.1"]
             [lein-nvd "1.4.1"]
-            [com.livingsocial/lein-dependency-check "1.1.4"]]
+            [com.livingsocial/lein-dependency-check "1.1.5"]]
   :jvm-opts ^:replace []}}
