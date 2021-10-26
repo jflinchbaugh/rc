25b290a833570aeca66fd014dae3e6b6b41f03da
John Flinchbaugh <john@hjsoft.com>
2021-10-26 13:44:29 -0400

    update plugins

--- profiles.clj
+++ profiles.clj
@@ -4,7 +4,7 @@
             [lein-cloverage "1.2.2"]
             [lein-ancient "0.7.0"]
             [lein-kibit "0.1.8"]
-            [com.jakemccrary/lein-test-refresh "0.24.1"]
-            [lein-nvd "1.4.1"]
+            [com.jakemccrary/lein-test-refresh "0.25.0"]
+            [lein-nvd "1.5.0"]
             [com.livingsocial/lein-dependency-check "1.1.5"]]
   :jvm-opts ^:replace []}}
