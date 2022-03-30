ac4cef3d3cc1e6a79830872a73a314d85312792b
John Flinchbaugh <john@hjsoft.com>
2022-03-30 14:46:54 -0400

    clojure tooling update + antq

--- profiles.clj
+++ profiles.clj
@@ -1,10 +1,11 @@
 {:user
  {
   :plugins [
-            [lein-cloverage "1.2.2"]
+            [lein-cloverage "1.2.3"]
             [lein-ancient "0.7.0"]
             [lein-kibit "0.1.8"]
             [com.jakemccrary/lein-test-refresh "0.25.0"]
             [lein-nvd "2.0.0"]
+            [com.github.liquidz/antq "RELEASE"]
             [com.livingsocial/lein-dependency-check "1.3.0"]]
   :jvm-opts ^:replace []}}
