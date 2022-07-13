d387505a0194ed936254be6d3b56cfa6c0c76524
John Flinchbaugh <john@hjsoft.com>
2022-07-13 16:18:57 -0400

    update cloverage

--- profiles.clj
+++ profiles.clj
@@ -1,7 +1,7 @@
 {:user
  {
   :plugins [
-            [lein-cloverage "1.2.3"]
+            [lein-cloverage "1.2.4"]
             [lein-ancient "0.7.0"]
             [lein-kibit "0.1.8"]
             [com.jakemccrary/lein-test-refresh "0.25.0"]
